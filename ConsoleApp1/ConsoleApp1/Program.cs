e57fb4194f5fa36fa3ab3850c79daa2de4152dd9
Matthias Henning <henning@geomarketing.de>
2025-08-18 16:55:26 +0200

    weiter

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace ConsoleApp1
+﻿using System.Collections.Generic;
+using Implementation;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var first = new First();
         }
     }
 }
