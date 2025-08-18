09077fdc43b36d33dcdb6c51003cd4093439d7ba
Matthias Henning <henning@geomarketing.de>
2025-08-18 16:55:40 +0200

    weiter

---
+++ ConsoleApp1/Implemementation/First.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace Implementation
+{
+    public class First : IFirst
+    {
+        public void DoIt()
+        {
+            var a = 321 + 2;
+            throw new NotImplementedException();
+        }
+    }
+}
+ 
